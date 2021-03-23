b6f467579059d84d8755028d60d1a2cf7f2fe05f
erthru <ersaka96@gmail.com>
2021-03-23 22:45:07 +0800

    fix nav key on search

--- src/containers/search/index.tsx
+++ src/containers/search/index.tsx
@@ -2,15 +2,22 @@ import { Box } from "@chakra-ui/layout";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import { useDispatch } from "react-redux";
 import { useLocation } from "react-router";
 import CDatasets from "../../components/datasets";
 import CGridVisualizations from "../../components/grid-visualizations";
 import CInfographics from "../../components/infograpics";
+import { setNavigationKey } from "../../data/store/navigation/actions";
 import { TITLE } from "../../helpers/environments";
 
 const XSearch = () => {
     const query = new URLSearchParams(useLocation().search);
     const [keywords, setKeywords] = useState("");
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setNavigationKey(null));
+    }, []);
 
     useEffect(() => {
         if (query.get("query") !== null) setKeywords(query.get("query")!!);
